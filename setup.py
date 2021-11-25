253967d9743f3a32805f1636f31b88bd77050152
lxstvayne <ssavi2002@yandex.ru>
2021-11-25 20:08:11 +0300

    add classifiers

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
 from setuptools import setup, find_packages
 
-
-__version__ = '1.0.1'
+__version__ = '2.1.0'
 
 setup(name='vk_maria',
       version=__version__,
-      description='vk bot api wrapper',
+      description='vk bot api framework',
       long_description=open('README.md', encoding='utf-8').read(),
       long_description_content_type="text/markdown",
       url='https://github.com/lxstvayne/vk_maria',
       keywords='vk bot tools',
-      packages=find_packages(),
+      packages=find_packages(exclude=('tests',)),
       install_requires=[
-            'requests>=2.20.1',
-            'loguru>=0.5.3',
-            'pydotdict>=3.3.11'
+          'requests>=2.20.1',
+          'loguru>=0.5.3',
+          'pydotdict>=3.3.11'
+      ],
+      requires_python='>=3.7',
+      classifiers=[
+          'Environment :: Console',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Topic :: Software Development :: Libraries :: Application Frameworks'
       ],
       author='lxstvayne',
       author_email='lxstv4yne@gmail.com',
