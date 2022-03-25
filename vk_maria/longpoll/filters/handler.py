80c2d9bb44fe84eb91fdfb5c8db0ba700f6406f2
lostvayne <ssavi2002@yandex.ru>
2022-03-25 20:51:38 +0300

    Сделал интроспекцию параметров функции и биндинг их к хендлеру.

--- vk_maria/longpoll/filters/handler.py
+++ vk_maria/longpoll/filters/handler.py
@@ -1,4 +1,6 @@
 import typing
+import inspect
+from functools import partial
 
 from .filters import Filters, FiltersFactory, AbstractFilter
 from ..fsm import FSMContext
@@ -9,14 +11,21 @@ class HandlerObject:
     def __init__(self, function: callable, *filters: AbstractFilter):
         self.function = function
         self.filters = Filters(*filters)
+        self._bind_args()
 
     def test_handler(self, event: Event) -> bool:
         return self.filters.check_all(event)
 
-    def __call__(self, *args):
-        if self.function.__code__.co_argcount == 2:
-            args = (args[0], FSMContext.get_current())
+    def _bind_args(self):
+        params = inspect.signature(self.function).parameters
+        bind_args = {}
+        for param_name, annotation in params.items():
+            if annotation.annotation == FSMContext:
+                bind_args[param_name] = FSMContext.get_current()
 
+        self.function = partial(self.function, **bind_args)
+
+    def __call__(self, *args):
         return self.function(*args)
 
 
@@ -28,10 +37,5 @@ class HandlerManager:
                          function: callable,
                          *filters,
                          **bound_filters):
-        # reg_bound_filters = (FiltersFactory.get_filter_by_key(key, filter_value)
-        #                      for key, filter_value in bound_filters.items())
-
-        # reg_filters = (*reg_bound_filters, *(custom_filter() for custom_filter in filters))
         reg_filters = FiltersFactory.get_filters(*filters, **bound_filters)
-
         self.handlers.append(HandlerObject(function, *reg_filters))
