68d49687a5159debe9b5b2f91440536c17216a52
lxstvayne <ssavi2002@yandex.ru>
2023-04-25 22:22:04 +0300

    version 3.0.8

--- examples/upload_photo.py
+++ examples/upload_photo.py
@@ -1,14 +1,17 @@
-from vk_maria import Vk
+from vk_maria import Vk, types
 from vk_maria.upload import Upload
 
 
 def main():
     """Пример загрузки и отправки фотографии в личное сообщение"""
 
-    vk = Vk(access_token='token')
+    vk = Vk(access_token='TOKEN')
     upload = Upload(vk)
 
-    photo = upload.photo('your_photo.png')  # Или upload.photo(open('your_photo.png', 'rb'))
+    image = types.FileSystemInputFile('./image.jpg')
+    # Or
+    # image = types.BufferedInputFile(filename, BytesIO)
+    photo = upload.photo(image)
 
     vk.messages_send(user_id=1234567890, attachment=photo)
 
